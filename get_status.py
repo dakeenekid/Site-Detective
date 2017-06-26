f06e58703af01245796707723435709395ed1b80
dakeenekid <dbkeene.tsyc@gmail.com>
2017-06-25 20:35:59 -0400

    First commit

---
+++ get_status.py
@@ -0,0 +1,29 @@
+import urllib
+
+def get_status():
+    site = raw_input("Which website do you want to check the status of?")
+    print("You are finding the status of "+site+".")
+
+    page = urllib.urlopen("http://www."+site+".com").getcode()
+
+    if page == 200:
+        print page
+        return 'The page is up!'
+
+
+    elif page == 404:
+        print page
+        return 'Page is down!'
+
+    elif page == 503:
+        print page
+        return 'Page is down!'
+    elif page == 429:
+        print 'Please stop spamming this server with requests'
+
+    else:
+        print page
+        return 'Page is down!'
+
+
+get_status()
